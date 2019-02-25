5f2bd9920e980ceaaaca1ae302da5b57d3eb67ff
i7p9h9 <i7p9h9@gmail.com>
2019-02-25 08:50:29 +0300

    add wide and slim versions

--- swishnet.py
+++ swishnet.py
@@ -29,43 +29,43 @@ def __causal_gated_conv1D(x=None, filters=16, length=6, strides=1):
         return causal_gated_conv1D(x=x, filters=filters, length=length, strides=strides)
 
 
-def SwishNet(input_shape, classes):
+def SwishNet(input_shape, classes, width_multiply=1):
     _x_in = layers.Input(shape=input_shape)
 
     # 1 block
-    _x_up = __causal_gated_conv1D(filters=16, length=3)(_x_in)
-    _x_down = __causal_gated_conv1D(filters=16, length=6)(_x_in)
+    _x_up = __causal_gated_conv1D(filters=16 * width_multiply, length=3)(_x_in)
+    _x_down = __causal_gated_conv1D(filters=16 * width_multiply, length=6)(_x_in)
     _x = layers.Concatenate()([_x_up, _x_down])
 
     # 2 block
-    _x_up = __causal_gated_conv1D(filters=8, length=3)(_x)
-    _x_down = __causal_gated_conv1D(filters=8, length=6)(_x)
+    _x_up = __causal_gated_conv1D(filters=8 * width_multiply, length=3)(_x)
+    _x_down = __causal_gated_conv1D(filters=8 * width_multiply, length=6)(_x)
     _x = layers.Concatenate()([_x_up, _x_down])
 
     # 3 block
-    _x_up = __causal_gated_conv1D(filters=8, length=3)(_x)
-    _x_down = __causal_gated_conv1D(filters=8, length=6)(_x)
+    _x_up = __causal_gated_conv1D(filters=8 * width_multiply, length=3)(_x)
+    _x_down = __causal_gated_conv1D(filters=8 * width_multiply, length=6)(_x)
     _x_concat = layers.Concatenate()([_x_up, _x_down])
 
     _x = layers.Add()([_x, _x_concat])
 
     # 4 block
-    _x_loop1 = __causal_gated_conv1D(filters=16, length=3, strides=3)(_x)
+    _x_loop1 = __causal_gated_conv1D(filters=16 * width_multiply, length=3, strides=3)(_x)
     _x = layers.Add()([_x, _x_loop1])
 
     # 5 block
-    _x_loop2 = __causal_gated_conv1D(filters=16, length=3, strides=2)(_x)
+    _x_loop2 = __causal_gated_conv1D(filters=16 * width_multiply, length=3, strides=2)(_x)
     _x = layers.Add()([_x, _x_loop2])
 
     # 6 block
-    _x_loop3 = __causal_gated_conv1D(filters=16, length=3, strides=2)(_x)
+    _x_loop3 = __causal_gated_conv1D(filters=16 * width_multiply, length=3, strides=2)(_x)
     _x = layers.Add()([_x, _x_loop3])
 
     # 7 block
-    _x_forward = __causal_gated_conv1D(filters=16, length=3, strides=2)(_x)
+    _x_forward = __causal_gated_conv1D(filters=16 * width_multiply, length=3, strides=2)(_x)
 
     # 8 block
-    _x_loop4 = __causal_gated_conv1D(filters=32, length=3, strides=2)(_x)
+    _x_loop4 = __causal_gated_conv1D(filters=32 * width_multiply, length=3, strides=2)(_x)
 
     # output
     _x = layers.Concatenate()([_x_loop2, _x_loop3, _x_forward, _x_loop4])
@@ -76,6 +76,14 @@ def SwishNet(input_shape, classes):
     model = models.Model(inputs=_x_in, outputs=_x)
 
     return model
+
+
+def SwishNetWide(input_shape, classes):
+    return SwishNet(input_shape=input_shape, classes=classes, width_multiply=2)
+
+
+def SwishnetSlim(input_shape, classes):
+    return SwishNet(input_shape=input_shape, classes=classes, width_multiply=1)
 
 
 if __name__ == "__main__":
